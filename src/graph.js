523fd16e40baa33c7f20c6e53cba83240af2c062
Alan Jiang <adj2424@gmail.com>
2022-10-25 15:45:32 -0400

    hi

--- src/graph.js
+++ src/graph.js
@@ -1,10 +1,8 @@
-import './App.css';
-import { Graph } from './graph';
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import { ArgumentAxis, ValueAxis, Chart, BarSeries } from '@devexpress/dx-react-chart-material-ui';
 
-function App() {
+export const Graph = () => {
   const data = [
     { argument: 'Monday', value: 30 },
     { argument: 'Tuesday', value: 20 },
@@ -13,16 +11,12 @@ function App() {
     { argument: 'Friday', value: 60 }
   ];
   return (
-    <div>
-      <Paper>
-        <Chart data={data}>
-          <ArgumentAxis />
-          <ValueAxis />
-          <BarSeries valueField="value" argumentField="argument" />
-        </Chart>
-      </Paper>
-    </div>
+    <Paper>
+      <Chart data={data}>
+        <ArgumentAxis />
+        <ValueAxis />
+        <BarSeries valueField="value" argumentField="argument" />
+      </Chart>
+    </Paper>
   );
-}
-
-export default App;
+};
